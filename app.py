e08ac8935d48e24152f3d3e0618a6c4f8fdd4d18
yannickRafael <yannickrafael286@gmail.com>
2025-05-31 20:25:20 +0200

    registered curso blue print

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from routes.estudante_route import estudante_bp
+from routes.curso_route import curso_bp
 
 app = Flask(__name__) 
 
 app.register_blueprint(estudante_bp, url_prefix='/estudante')
+app.register_blueprint(curso_bp, url_prefix='/curso')
 
 @app.route('/', methods=['GET'])
 def home():
